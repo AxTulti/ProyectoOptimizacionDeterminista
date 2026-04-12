9f5a6178a3cd3a890639e71311680166d234b116
Angel Nicolas Landa Tapia <a01668133@tec.mx>
2026-04-12 12:24:48 -0600

    comment additions

--- segmentation.py
+++ segmentation.py
@@ -7,13 +7,13 @@ def gender_segmentation(df: pd.DataFrame, genders: list) -> pd.DataFrame:
     """
     gender_segmented_df = df.copy()
 
+    # Normalizing genders and ensuring values
     normalized_genders = [gen.strip().lower() for gen in genders]
     real_genders = ["masculino", "femenino", "otro"]
-
     if any(gen not in real_genders for gen in normalized_genders):
         raise ValueError(f"Valid options: {real_genders}")
 
-    #
+    # Filtering
     gender_segmented_df = gender_segmented_df[
         gender_segmented_df["Genero"].astype(str).str.strip().str.lower()
         .isin(normalized_genders)
@@ -28,6 +28,7 @@ def age_segmentation(df: pd.DataFrame, segments: list) -> pd.DataFrame:
     """
     age_segmented_df = df.copy()
 
+    # Filtering
     age_segmented_df = age_segmented_df[
         age_segmented_df["Edad"].astype(str).str.strip().isin(segments)
     ]
